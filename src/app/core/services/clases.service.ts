7c7ad9e660d9a3edc7a7d51be718736825374c61
SimplementeDiego <diegodellepianefurrer@gmail.com>
2023-09-03 18:02:55 -0300

    Avance

--- src/app/core/services/clases.service.ts
+++ src/app/core/services/clases.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 import { baseUrl } from 'src/environments/environments';
 import { AlumnosService } from './alumnos.service';
 import { MatDialog } from '@angular/material/dialog';
 import { PopupComponent } from 'src/app/shared/components/popup/popup.component';
+import { CursosService } from './cursos.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +15,12 @@ export class ClasesService {
 
   displayedColumns: string[] = ['id', 'alumnoInscripcion', 'cursoInscripcion','action'];
 
+  private _inscripciones$ = new BehaviorSubject<any | null>(null);
+  public inscripciones$ = this._inscripciones$.asObservable();
+
   clases = [];
 
-  constructor( private _http: HttpClient, private _alumnosService: AlumnosService, private dialog: MatDialog ) {
+  constructor( private _http: HttpClient, private _alumnosService: AlumnosService, private dialog: MatDialog, private _cursosService: CursosService ) {
 
   }
 
@@ -30,6 +34,42 @@ export class ClasesService {
 
   getClaseList(): Observable<any> {
     return this._http.get(`${baseUrl}clases`);
+  }
+
+  getCursosCompletos(data: any){
+
+    const cantidad = data.length;
+
+    let cursosConInfo: Array<any> = [];
+
+    this.getClaseList().subscribe({
+      next: (inscripciones: Array<any>)=>{
+
+        this._cursosService.getCursoList().subscribe({
+          next: (cursos: Array<any>)=>{
+
+            const filtrado = inscripciones.filter( (item)=> { if ( item.alumno == data.id){ return item } } )
+
+            filtrado.forEach( (inscripcion) => {
+
+              cursos.forEach( (curso) => {
+
+                if (curso.id==inscripcion.curso){
+                  cursosConInfo.push(curso);
+                }
+
+              });
+
+            });
+
+            this._inscripciones$.next(cursosConInfo);
+
+          }
+        });
+
+      }
+    });
+
   }
 
   deleteInscripcionesAlumno( id: number ){
--- src/app/dashboard/pages/alumnos/alumnos-info/alumnos-info.component.ts
+++ src/app/dashboard/pages/alumnos/alumnos-info/alumnos-info.component.ts
@@ -1,14 +1,34 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { ClasesService } from 'src/app/core/services/clases.service';
 
 @Component({
   selector: 'app-alumnos-info',
   templateUrl: './alumnos-info.component.html',
   styleUrls: ['./alumnos-info.component.css']
 })
-export class AlumnosInfoComponent {
+export class AlumnosInfoComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data:any  ,private ref: MatDialogRef<AlumnosInfoComponent>){
+  public cursosObservable: Observable<any>;
+
+  public cursos: any = [];
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data:any  ,private ref: MatDialogRef<AlumnosInfoComponent>, private _clasesService: ClasesService){
+
+    this.cursosObservable = this._clasesService.inscripciones$
+
+    this._clasesService.getCursosCompletos(data);
+
+  }
+
+  ngOnInit(): void {
+
+    this.cursosObservable.subscribe({
+      next: (res)=>{
+        this.cursos = res;
+      }
+    });
 
   }
 
